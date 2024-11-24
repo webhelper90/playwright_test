d2e73dd4849a2b268a0f3dfb4875b61c8327b13e
webhelper90 <webhelper90@gmail.com>
2024-11-24 16:59:05 +0900

    Update searchAnime.js

--- searchAnime.js
+++ searchAnime.js
@@ -1,4 +1,5 @@
 const { chromium } = require('playwright');
+const path = require('path');
 
 (async () => {
     const browser = await chromium.launch();
@@ -10,10 +11,10 @@ const { chromium } = require('playwright');
         await page.goto('https://www.google.com', { waitUntil: 'domcontentloaded' });
 
         // スクリーンショットを取得
-        //await page.screenshot({ path: 'google_screenshot1.png' });
-
+        await page.screenshot({ path: path.join(__dirname, 'google_screenshot1.png') });
+        
         // 検索ボックスが表示されるのを待つ
-        await page.waitForSelector('textarea[name="q"]', { timeout: 60000 });
+        await page.waitForSelector('textarea[name="q"]', { timeout: 30000 });
 
         // 検索ボックスに「アニメ」を入力
         await page.fill('textarea[name="q"]', 'アニメ');
@@ -22,12 +23,13 @@ const { chromium } = require('playwright');
         // 検索結果が表示されるのを待つ
         await page.waitForSelector('h3');
 
+        // スクリーンショットを取得
+        await page.screenshot({ path: path.join(__dirname, 'google_screenshot2.png') });
+
         // 検索結果を取得
         const results = await page.$$eval('h3', elements => elements.map(el => el.innerText));
 
-        // スクリーンショットを取得
-        await page.screenshot({ path: 'google_screenshot.png' });
-
+        
         console.log('検索結果:', results);
     } catch (error) {
         console.error('エラーが発生しました:', error);
