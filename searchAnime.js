b794e6b005d3384b511ca75ea67f3511f5a31bba
webhelper90 <webhelper90@gmail.com>
2024-11-24 16:17:53 +0900

    Update searchAnime.js

--- searchAnime.js
+++ searchAnime.js
@@ -7,8 +7,11 @@ const { chromium } = require('playwright');
 
     // Googleにアクセス
     await page.goto('https://www.google.com');
+    
+    // 検索ボックスが表示されるのを待つ
+    await page.waitForSelector('input[name="q"]', { timeout: 60000 });
 
-    // 検索ボックスに「アニメ」を入力して検索実行
+    // 検索ボックスに「アニメ」を入力
     await page.fill('input[name="q"]', 'アニメ');
     await page.keyboard.press('Enter');
 
