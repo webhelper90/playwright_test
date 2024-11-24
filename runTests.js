a69341e879052ce1702414f35a5fbce7c841f695
webhelper90 <webhelper90@gmail.com>
2024-11-24 19:11:59 +0900

    Update runTests.js

--- runTests.js
+++ runTests.js
@@ -1,4 +1,4 @@
-const { chromium, webkit, devices } = require('playwright');
+const { chromium, webkit, firefox devices } = require('playwright');
 const path = require('path');
 const fs = require('fs');
 
