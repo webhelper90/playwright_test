eef10a206605af47dd0c94cf85ecf2b328a6ea16
webhelper90 <webhelper90@gmail.com>
2024-11-24 17:05:10 +0900

    Update searchAnime.js

--- searchAnime.js
+++ searchAnime.js
@@ -1,5 +1,6 @@
 const { chromium } = require('playwright');
 const path = require('path');
+const fs = require('fs');
 
 (async () => {
     const browser = await chromium.launch();
@@ -7,24 +8,31 @@ const path = require('path');
     const page = await context.newPage();
 
     try {
+        // screen_shotディレクトリが存在しない場合は作成する
+        const screenshotDir = path.join(__dirname, 'screen_shot');
+        if (!fs.existsSync(screenshotDir)) {
+          fs.mkdirSync(screenshotDir);
+        }
+        
         // Googleにアクセス
         await page.goto('https://www.google.com', { waitUntil: 'domcontentloaded' });
 
-        // スクリーンショットを取得
-        await page.screenshot({ path: path.join(__dirname, 'google_screenshot1.png') });
-        
         // 検索ボックスが表示されるのを待つ
         await page.waitForSelector('textarea[name="q"]', { timeout: 30000 });
 
         // 検索ボックスに「アニメ」を入力
         await page.fill('textarea[name="q"]', 'アニメ');
+
+        // スクリーンショットを取得
+        await page.screenshot({ path: path.join(screenshotDir, 'google_screenshot1.png') });
+
         await page.keyboard.press('Enter');
 
         // 検索結果が表示されるのを待つ
         await page.waitForSelector('h3');
 
         // スクリーンショットを取得
-        await page.screenshot({ path: path.join(__dirname, 'google_screenshot2.png') });
+        await page.screenshot({ path: path.join(screenshotDir, 'google_screenshot2.png') });
 
         // 検索結果を取得
         const results = await page.$$eval('h3', elements => elements.map(el => el.innerText));
